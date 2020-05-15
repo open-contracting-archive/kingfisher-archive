d3f6f5ee157b0344ec6b2e645c7869999cab0e47
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-05-15 15:23:15 -0400

    Various improvements
    - Use rsync "archive" mode and compress files during transfer.
    - Delete empty directories on destination.
    - Don't echo a collection unless work is done.
    - Don't look in old data directory.
    - Restore host key checking.
    
    Fix style
    
    - Remove comments that repeat echo.
    - Change SQL letter case.
    - Fix indentation.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # kingfisher-archive
 
-Tools relating to the long-term storage of data downloaded using the (OCDS Kingfisher Platform)[https://ocdskingfisher.readthedocs.io/en/latest/]
+Tools relating to the long-term storage of data downloaded using the [OCDS Kingfisher Platform](https://ocdskingfisher.readthedocs.io/en/latest/)
--- rsync-downloaded-files.sh
+++ rsync-downloaded-files.sh
@@ -1,53 +1,33 @@
 #!/bin/bash
-
 set -e
 
 # Iterate over all the collections that are fully loaded into the database
 
-for collection in $(psql -U ocdskfpreadonly -d ocdskingfisherprocess -h localhost -c "select source_id, to_char(data_version, 'YYYYMMDD_HH24MISS') from collection where store_end_at is not null and transform_type = ''" -qAtX | tr "|" "/"); do
+for collection in $(psql -U ocdskfpreadonly -d ocdskingfisherprocess -h localhost -c "SELECT source_id, to_char(data_version, 'YYYYMMDD_HH24MISS') FROM collection WHERE store_end_at IS NOT NULL AND transform_type = ''" -qAtX | tr "|" "/"); do
+  # If the data has already been successfully archived, nothing is done.
+  if [ -d "/home/ocdskfs/scrapyd/data/${collection}/" ]; then 
+	  date
+	  echo "COLLECTION ${collection}"
 
-	# Run date so that when output appears in log we know when it happened
-	date
+    LOCAL_DIR="/home/ocdskfs/scrapyd/data/${collection}/"
+    echo "Found ${LOCAL_DIR}"
 
-        echo "COLLECTION ${collection}"
+    echo "Create folder on disk server"
+    ssh archive@archive.kingfisher.open-contracting.org "mkdir -p /home/archive/data/${collection}"
 
-        scraper=$(echo ${collection} | awk 'BEGIN { FS = "/" } { print $1 }')
-        timestamp=$(echo ${collection} | awk 'BEGIN { FS = "/" } { print $2 }')
+    echo "Rsync to the disk server"
+    rsync -az ${LOCAL_DIR} archive@archive.kingfisher.open-contracting.org:/home/archive/data/${collection}/ || continue
 
+    # Delete original files if rsync returned 0 (restricted to the right place by sudoers)
+    echo "Delete original files"
+    sudo -u ocdskfs rm -rf ${LOCAL_DIR}
 
-        # Find folder on source server
-	# LOCAL_DIR serves 2 purposes; the data may be in one of 2 places AND more importantly, if the data has already been successfully archived, nothing is done.
-	LOCAL_DIR=""
-	if [ -d "/home/ocdskfs/scrapyd/data/${collection}/" ]; then 
-		LOCAL_DIR="/home/ocdskfs/scrapyd/data/${collection}/"
-	elif [ -d "/home/ocdskfs/old-data/${collection}" ]; then 
-		LOCAL_DIR="/home/ocdskfs/old-data/${collection}/"
-	fi
-	
-        if [ "${LOCAL_DIR}" ]; then
+    echo "Compress target files"
+	  spider=$(echo ${collection} | awk 'BEGIN { FS = "/" } { print $1 }')
+	  timestamp=$(echo ${collection} | awk 'BEGIN { FS = "/" } { print $2 }')
+    ssh archive@archive.kingfisher.open-contracting.org "tar -Jcvf /home/archive/data/${spider}_${timestamp}.tar.xz /home/archive/data/$collection && rm -rf /home/archive/data/${collection} && find /home/archive/data/${collection} -empty -type d -delete"
 
-		echo "Found ${LOCAL_DIR}"
-
-		# Create folder on disk server
-		echo "Create folder on disk server"
-		ssh -o StrictHostKeyChecking=no archive@archive.kingfisher.open-contracting.org "mkdir -p /home/archive/data/${collection}"
-
-		# Rsync to the disk server
-		echo "Rsync to the disk server"
-		rsync -r -e "ssh -o StrictHostKeyChecking=no" ${LOCAL_DIR} archive@archive.kingfisher.open-contracting.org:/home/archive/data/${collection}/ || continue
-
-		# Delete original files if rsync returned 0 (restricted to the right place by sudoers)
-		echo "Delete original files"
-		sudo -u ocdskfs rm -rf ${LOCAL_DIR}
-
-		# Compress target files
-		echo "Compress target files"
-		ssh archive@archive.kingfisher.open-contracting.org "tar -Jcvf /home/archive/data/${scraper}_${timestamp}.tar.xz /home/archive/data/$collection && rm -rf /home/archive/data/${collection}"
-
-		# Only one of these processes should run at once. So once we have found and done some work, exit.
-		exit 1
-
-	fi
-
-done;
-
+    # Only one of these processes should run at once. So once we have found and done some work, exit.
+    exit 1
+  fi
+done
