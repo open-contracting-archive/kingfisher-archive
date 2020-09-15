8e4b1caa008addf11f9b3b542c14a9fbed1d7575
James <james.baster@opendataservices.coop>
2020-09-15 17:25:24 +0100

    database-process: Use psycopg2
    https://github.com/open-contracting/kingfisher-archive/pull/17

--- ocdskingfisherarchive/database_process.py
+++ ocdskingfisherarchive/database_process.py
@@ -1,31 +1,27 @@
-import sqlalchemy as sa
+import psycopg2
+
 from ocdskingfisherarchive.collection import Collection
 
 
 class DataBaseProcess:
 
     def __init__(self, config):
+        self.connection = psycopg2.connect(**config.get_database_connection_params())
+        self.cursor = self.connection.cursor()
         self.config = config
-        self._engine = None
-
-    def get_engine(self):
-        if not self._engine:
-            self._engine = sa.create_engine(
-                self.config.database_uri,
-            )
-        return self._engine
 
     def get_collections_to_consider_archiving(self):
-        sql = "SELECT * FROM collection WHERE sample IS FALSE AND store_end_at IS NOT NULL AND transform_type = ''"
+        sql = "SELECT id, source_id, data_version  FROM collection " +\
+              "WHERE sample IS FALSE AND store_end_at IS NOT NULL AND transform_type = ''"
         collections = []
-        with self.get_engine().begin() as connection:
-            for row in connection.execute(sa.sql.expression.text(sql)):
-                collections.append(
-                    Collection(
-                        config=self.config,
-                        database_id=row['id'],
-                        source_id=row['source_id'],
-                        data_version=row['data_version'],
-                    )
+        self.cursor.execute(sql)
+        for row in self.cursor.fetchall():
+            collections.append(
+                Collection(
+                    config=self.config,
+                    database_id=row[0],
+                    source_id=row[1],
+                    data_version=row[2],
                 )
+            )
         return collections
