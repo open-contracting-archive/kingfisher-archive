553ba8aea9f5e36e75acc040feceb217e0e26e5a
James <james.baster@opendataservices.coop>
2020-10-02 09:48:23 +0100

    Detect if Scrapy log is finished or not
    Includes catching SIGTERMs
    
    https://github.com/open-contracting/kingfisher-archive/issues/32

--- tests/test_scrapy_log_file.py
+++ tests/test_scrapy_log_file.py
@@ -15,6 +15,14 @@ FILENAME_LOG_FROM_DATE1 = os.path.join(os.path.dirname(
         os.path.realpath(__file__)), 'data', 'log_from_date1.log'
     )
 
+FILENAME_LOG_SIGINT1 = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), 'data', 'log_sigint1.log'
+    )
+
+FILENAME_LOG_IN_PROGRESS1 = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), 'data', 'log_in_progress1.log'
+    )
+
 
 def test_does_match_date_version():
     scrapy_log_file = ScrapyLogFile(FILENAME_LOG1)
@@ -56,3 +64,28 @@ def test_is_subset_sample_1():
 def test_is_subset_from_date_1():
     scrapy_log_file = ScrapyLogFile(FILENAME_LOG_FROM_DATE1)
     assert True == scrapy_log_file.is_subset()  # noqa: E712
+
+
+def test_is_finished_1():
+    scrapy_log_file = ScrapyLogFile(FILENAME_LOG1)
+    assert True == scrapy_log_file.is_finished()  # noqa: E712
+
+
+def test_is_finished_sample_1():
+    scrapy_log_file = ScrapyLogFile(FILENAME_LOG_SAMPLE1)
+    assert True == scrapy_log_file.is_finished()  # noqa: E712
+
+
+def test_is_finished_from_date_1():
+    scrapy_log_file = ScrapyLogFile(FILENAME_LOG_FROM_DATE1)
+    assert True == scrapy_log_file.is_finished()  # noqa: E712
+
+
+def test_is_finished_sigint_1():
+    scrapy_log_file = ScrapyLogFile(FILENAME_LOG_SIGINT1)
+    assert False == scrapy_log_file.is_finished()  # noqa: E712
+
+
+def test_is_finished_in_progress_1():
+    scrapy_log_file = ScrapyLogFile(FILENAME_LOG_IN_PROGRESS1)
+    assert False == scrapy_log_file.is_finished()  # noqa: E712
