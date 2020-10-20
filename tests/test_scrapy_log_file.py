d2fde5b0cbc831160f4c0e0f465793dea91746b0
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-10-19 20:31:35 -0400

    Complete ScrapyLogFile tests

--- tests/test_scrapy_log_file.py
+++ tests/test_scrapy_log_file.py
@@ -5,6 +5,69 @@ import pytest
 from ocdskingfisherarchive.scrapy_log_file import ScrapyLogFile
 from tests import path
 
+data_version = datetime.datetime(2020, 1, 2, 3, 4, 5)
+message = '2020-01-02 03:04:05 [scrapy.utils.log] INFO message'
+
+
+@pytest.mark.parametrize('files, expected', [
+    # Only match.
+    ({'test1.log': '2020-01-02 03:04:02 [scrapy.utils.log] INFO message',
+      'test2.log': '2020-01-02 03:04:05 [scrapy.utils.log] INFO message'}, 'test2.log'),
+    # First match.
+    ({'test1.log': '2020-01-02 03:04:03 [scrapy.utils.log] INFO message',
+      'test2.log': '2020-01-02 03:04:05 [scrapy.utils.log] INFO message'}, 'test1.log'),
+    ({'test1.log': '2020-01-02 03:04:05 [scrapy.utils.log] INFO message',
+      'test2.log': '2020-01-02 03:04:03 [scrapy.utils.log] INFO message'}, 'test1.log'),
+    # No match.
+    ({'test1.log': '2020-01-02 03:04:06 [scrapy.utils.log] INFO message'}, None),
+])
+def test_find(files, expected, tmpdir):
+    directory = tmpdir.mkdir('source_id')
+    for filename, content in files.items():
+        file = directory.join(filename)
+        file.write(content)
+
+    if expected:
+        assert ScrapyLogFile.find(tmpdir, 'source_id', data_version).name == directory.join(expected)
+    else:
+        assert ScrapyLogFile.find(tmpdir, 'source_id', data_version) is None
+
+
+def test_find_not_existing(tmpdir):
+    assert ScrapyLogFile.find(tmpdir, 'source_id', data_version) is None
+
+
+def test_find_not_directory(tmpdir):
+    file = tmpdir.join('source_id')
+    file.write(message)
+
+    assert ScrapyLogFile.find(tmpdir, 'source_id', data_version) is None
+
+
+def test_find_not_extension(tmpdir):
+    directory = tmpdir.mkdir('source_id')
+    file = directory.join('file.ext')
+    file.write(message)
+
+    assert ScrapyLogFile.find(tmpdir, 'source_id', data_version) is None
+
+
+@pytest.mark.parametrize('filenames', [
+    ('test.log',),
+    ('test.log.stats',),
+    ('test.log', 'test.log.stats'),
+])
+def test_delete(filenames, tmpdir):
+    for filename in filenames:
+        log = tmpdir.join(filename)
+        log.write('content')
+
+    scrapy_log_file = ScrapyLogFile(tmpdir.join('test.log'))
+    scrapy_log_file.delete()
+
+    for filename in filenames:
+        assert not tmpdir.join(filename).exists()
+
 
 @pytest.mark.parametrize('datetime, expected', [
     (datetime.datetime(2020, 9, 2, 5, 24, 55), False),
@@ -19,19 +82,6 @@ def test_match(datetime, expected):
     assert ScrapyLogFile(path('log_error1.log')).match(datetime) is expected
 
 
-def test_errors_count():
-    assert ScrapyLogFile(path('log_error1.log')).errors_count == 1
-
-
-@pytest.mark.parametrize('filename, expected', [
-    ('log_error1.log', False),
-    ('log_sample1.log', True),
-    ('log_from_date1.log', True),
-])
-def test_is_subset(filename, expected):
-    assert ScrapyLogFile(path(filename)).is_subset() is expected
-
-
 @pytest.mark.parametrize('filename, expected', [
     ('log_error1.log', True),
     ('log_sample1.log', True),
@@ -41,3 +91,20 @@ def test_is_subset(filename, expected):
 ])
 def test_is_finished(filename, expected):
     assert ScrapyLogFile(path(filename)).is_finished() is expected
+
+
+@pytest.mark.parametrize('filename, expected', [
+    ('log1.log', 0),
+    ('log_error1.log', 1),
+])
+def test_errors_count(filename, expected):
+    assert ScrapyLogFile(path(filename)).errors_count == expected
+
+
+@pytest.mark.parametrize('filename, expected', [
+    ('log_error1.log', False),
+    ('log_sample1.log', True),
+    ('log_from_date1.log', True),
+])
+def test_is_subset(filename, expected):
+    assert ScrapyLogFile(path(filename)).is_subset() is expected
